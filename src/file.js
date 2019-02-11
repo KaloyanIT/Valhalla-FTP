5a027732265c3af70417f8a0742c80203e8247d6
Kaloyan Kostov <kaloyanit@yahoo.com>
2019-02-12 00:41:05 +0200

    Check paths

--- src/file.js
+++ src/file.js
@@ -1,9 +1,44 @@
 const path = require("path");
 const fs = require("fs");
 const util = require("util");
+const $ = require('jquery')
 let spawn = require("child_process").spawn;
-
+console.log(path.resolve('This PC'))
+var os = require("os");
+var root = (os.platform() == "win32") ? process.cwd().split(path.sep)[0] : "/"
+console.log(root);
+// let rootDirs = [];
 let rootDirs = [];
+
+new Promise(function (resolve, reject) {  
+	let list = spawn("cmd");
+
+	list.stdout.on("data", function(data) {
+	  rootDirs.push(data.toString());
+	  
+	  console.log("stdout: " + data);
+	});
+  
+	list.stdin.write("wmic logicaldisk get name\n");
+	list.stdin.end();
+
+	setTimeout(() => {
+		resolve(rootDirs)
+	}, 3000)
+
+	// resolve(rootDirs)
+}).then(function (rootDirs) { 
+	let filesContainerClass = ".source-fileSystem";
+  let directoriesUlClass = '.drives';
+
+  console.log('display drives', rootDirs);
+	let element = ''
+  for(let i = 0; i < rootDirs.length; i++) {
+	  element += `<li>${rootDirs[i]}</li>`
+  }
+
+  $(directoriesUlClass).html(element)
+ });
 
 function loadFiles() {
   let list = spawn("cmd");
@@ -16,6 +51,7 @@ function loadFiles() {
 
   list.stdin.write("wmic logicaldisk get name\n");
   list.stdin.end();
+  
 
   displayDrives(rootDirs);
 }
